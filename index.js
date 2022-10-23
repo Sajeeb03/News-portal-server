c249a614e8b1f227236295d33766be4b00a8f751
Sajeb03 <sajeebmuntasir0@gmail.com>
2022-10-23 11:22:02 +0600

    done

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 app.use(cors())
 
 app.get('/', (req, res) => {
@@ -12,6 +13,28 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) => {
     res.send(categories)
 })
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === "08") {
+        res.send(news);
+    }
+    else {
+        const selectedCategory = news.filter(n => n.category_id === id);
+        res.send(selectedCategory)
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
 app.listen(port, () => {
-    console.log("newsportal running in port", port)
+    console.log("news portal running in port", port)
 })
